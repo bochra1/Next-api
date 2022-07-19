b1bd31ec783a2de6519b7f56ca4efbad57fcd517
hatem <hatem.diouani@sastec-group.com>
2022-07-19 11:48:35 +0100

    second commit

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -1,17 +1,15 @@
-import React from 'react'
 import { useState } from 'react'
-import { comments } from '../../data/comments'
-export default function CommentsPage() {
-const [comments, setComments]= useState([])
-const [comment, setComment] = useState('')
+function CommentsPage() {
+  const [comments, setComments] = useState([])
+  const [comment, setComment] = useState('')
 
-   const fetchComments = async() => { 
+  const fetchComments = async () => {
     const response = await fetch('/api/comments')
-   const data = await response.json()
-   setComments(data)
-}
+    const data = await response.json()
+    setComments(data)
+  }
 
-const submitComment = async () => {
+  const submitComment = async () => {
     const response = await fetch('/api/comments', {
       method: 'POST',
       body: JSON.stringify({ comment }),
@@ -22,13 +20,37 @@ const submitComment = async () => {
     const data = await response.json()
     console.log(data)
   }
+
+  const deleteComment = async commentId => {
+    const response = await fetch(`/api/comments/${commentId}`, {
+      method: 'DELETE'
+    })
+    const data = await response.json()
+    console.log(data)
+    fetchComments()
+  }
   return (
-<><div>
-<input type='texte' value={comment} onChange={(e) => setComment(e.target.value)}/>
-<button onClick={submitComment}>Submit Comment</button></div>
-<button onClick={fetchComments}>Load comments</button>
-{ comments.map(comment =>{
-    return <div key={comment.id}>{comment.id} {comment.text}</div>
-})}
-</> )
+    <>
+      <div>
+        <input
+          type='text'
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+        />
+        <button onClick={submitComment}>Submit comment</button>
+      </div>
+      <hr />
+      <button onClick={fetchComments}>Load comments</button>
+      {comments.map(comment => {
+        return (
+          <div key={comment.id}>
+            {comment.id}. {comment.text}
+            <button onClick={() => deleteComment(comment.id)}>Delete</button>
+          </div>
+        )
+      })}
+    </>
+  )
 }
+
+export default CommentsPage
